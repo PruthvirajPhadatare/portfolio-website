e92ea07783ffc1c95984a24f2cf93fba76362976
Pruthviraj <Pruthviraj@gamil.com>
2025-09-20 14:56:44 +0530

    Changes created

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 document.querySelectorAll('nav a').forEach(anchor => {
   anchor.addEventListener('click', function(e){
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior:'smooth' });
   });
 });
